d18d2d6b932fbc704178b594deee4352ca56939e
YashPanchal1012 <yashpanchal1012@gmail.com>
2025-07-14 14:48:13 -0500

    merge

--- recipes/static/recipes/home.js
+++ recipes/static/recipes/home.js
@@ -41,7 +41,7 @@ function Catalog() {
     <div className="row justify-content-center mx-4">
       <h1 className="text-center text-primary-emphasis my-3">Browse Recipes</h1>
       {recipes.map(recipe => (
-        <div className="card my-3 mx-4 col-md-3 col-lg-2 col-sm-6 bg-warning-subtle" key={recipe.id}>
+        <div className="card my-3 mx-4 col-md-3 col-lg-2 col-sm-6 bg-warning-subtle bg-gradient" key={recipe.id}>
           <img src={recipe.image_url} className="card-img-top img-fluid mt-3" alt={recipe.title} />
           <div className="card-body">
             <h5 className="card-title">{recipe.title}</h5>
